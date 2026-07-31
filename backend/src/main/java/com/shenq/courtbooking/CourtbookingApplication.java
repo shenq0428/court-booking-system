55bea69c17c6736e494d81435d122f1d1bf39821
ZS HomePC <wzshenq0428@gmail.com>
2026-07-31 23:40:16 +0800

    HELLO JAVA WORLD

---
+++ backend/src/main/java/com/shenq/courtbooking/CourtbookingApplication.java
@@ -0,0 +1,13 @@
+package com.shenq.courtbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtbookingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shenq/courtbooking/CourtbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenq.courtbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
